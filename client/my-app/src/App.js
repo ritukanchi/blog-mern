dbad8f701ed6f331186a500fa287e69f68fc6f22
Ritu Kanchi <ritukanchi04@gmail.com>
2024-04-01 02:30:41 +0530

    route-created

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,37 +1,20 @@
 import './App.css';
-
+import post from "./post"; 
+import header from "./header"; 
 function App() {
   return (
-    <main>
-        <header>
-          <a href="" classname="logo">My blog</a>
-          <nav>
-            <a href="">login</a>
-            <a href="">register</a>
-          </nav>
-        </header>
-        <div classname="post">
-          <img src="https://www.shutterstock.com/image-photo/bangalore-skyline-metro-city-image-260nw-1547756567.jpg" alt=""/>
-          <div classname="texts">
-            <h2>Bengaluru Skyline </h2>
-            <p>The Bengaluru skyline is a captivating fusion of modernity and tradition, reflecting the dynamic growth and development of India's Silicon Valley. As one of the fastest-growing cities in Asia, Bengaluru's skyline has undergone significant transformation over the past few decades, evolving from a primarily low-rise landscape to one dotted with towering skyscrapers, gleaming office complexes, and luxury residential towers.</p>   
-          </div>
-        </div>
-        <div classname="post">
-          <img src="https://www.shutterstock.com/image-photo/bangalore-skyline-metro-city-image-260nw-1547756567.jpg" alt=""/>
-          <div classname="texts">
-            <h2>Bengaluru Skyline </h2>
-            <p>The Bengaluru skyline is a captivating fusion of modernity and tradition, reflecting the dynamic growth and development of India's Silicon Valley. As one of the fastest-growing cities in Asia, Bengaluru's skyline has undergone significant transformation over the past few decades, evolving from a primarily low-rise landscape to one dotted with towering skyscrapers, gleaming office complexes, and luxury residential towers.</p>   
-          </div>
-        </div>
-        <div classname="post">
-          <img src="https://www.shutterstock.com/image-photo/bangalore-skyline-metro-city-image-260nw-1547756567.jpg" alt=""/>
-          <div classname="texts">
-            <h2>Bengaluru Skyline </h2>
-            <p>The Bengaluru skyline is a captivating fusion of modernity and tradition, reflecting the dynamic growth and development of India's Silicon Valley. As one of the fastest-growing cities in Asia, Bengaluru's skyline has undergone significant transformation over the past few decades, evolving from a primarily low-rise landscape to one dotted with towering skyscrapers, gleaming office complexes, and luxury residential towers.</p>   
-          </div>
-        </div>
-    </main>
+    <Routes>
+      <Route index element={<main>
+       <header/> 
+        <post/>
+        <post/> 
+        <post/>
+    </main>}/>
+    <Route path={'/login'} element={
+      <div>login</div>
+    }/>
+    </Routes>
+    
   );
 }
 
---
+++ client/my-app/src/header.js
@@ -0,0 +1,11 @@
+export default function header(){
+    return(
+        <header>
+          <a href="" classname="logo">My blog</a>
+          <nav>
+            <a href="/login">login</a>
+            <a href="">register</a>
+          </nav>
+        </header>
+    )
+}
