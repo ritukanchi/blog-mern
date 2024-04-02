985c70a5e86a167d75acf5fd119934c697014a23
Ritu Kanchi <ritukanchi04@gmail.com>
2024-04-03 01:18:04 +0530

    mongo connected

--- api/index.js
+++ api/index.js
@@ -1,10 +1,18 @@
 const express=require('express');
 const cors=require('cors');
+const mongoose=require("mongoose"); 
 const app=express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://kushaliv147:ZSMk1mJVPbIshpeH@cluster0.wjrxybm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'); 
 
 app.post('/register',(req,res)=> {
     const{username, password}=req.body; 
-    res.json('test ok')
+    res.json({requestData:{username,password}});
 });
 
 app.listen(4000); 
+//ZSMk1mJVPbIshpeH
+//mongodb+srv://kushaliv147:ZSMk1mJVPbIshpeH@cluster0.wjrxybm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- client/my-app/src/header.js
+++ client/my-app/src/header.js
@@ -1,7 +1,7 @@
 import {Link} from "react-router-dom";
 export default function Header(){
     return(<header>
-      <Link to="/" classname="logo">My blog</Link>
+      <Link to="/" className="logo">My blog</Link>
       <nav>
         <Link to="/login">login</Link>
         <Link to="/register">register</Link>
